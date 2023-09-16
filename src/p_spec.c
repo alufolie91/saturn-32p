d82eeb7c0bc90a0b2eb22a46241fc6e3ff6feacb
lug420 <nyabby420@gmail.com>
2023-09-16 02:48:26 +0200

    Don't call map load trigger linedefs when joining or reloading gamestate
    may fix desynchs on some maps

--- src/p_spec.c
+++ src/p_spec.c
@@ -6742,7 +6742,8 @@ void P_SpawnSpecials(INT32 fromnetsave)
 		}
 	}
 
-	P_RunLevelLoadExecutors();
+	if (!fromnetsave)
+		P_RunLevelLoadExecutors();
 }
 
 /** Adds 3Dfloors as appropriate based on a common control linedef.
