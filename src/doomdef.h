0894fb2544d75177f5453acedaa92f7c49220f51
alufolie91 <alufoliehorst@gmail.com>
2023-10-01 18:59:39 +0200

    change version string just in case

--- src/doomdef.h
+++ src/doomdef.h
@@ -149,8 +149,8 @@ extern char  logfilename[1024];
 #else
 #define VERSION    1 // Game version
 #define SUBVERSION 69 // more precise version number
-#define VERSIONSTRING "Saturn v3 - Uranus Fork v1.4"
-#define VERSIONSTRINGW L"Saturn v3 - Uranus Fork v1.4"
+#define VERSIONSTRING "Saturn v3 - Uranus Fork v1.5-rc1"
+#define VERSIONSTRINGW L"Saturn v3 - Uranus Fork v1.5-rc1"
 // Hey! If you change this, add 1 to the MODVERSION below! Otherwise we can't force updates!
 // And change CMakeLists.txt (not src/, but in root), for CMake users!
 // AND appveyor.yml, for the build bots!
