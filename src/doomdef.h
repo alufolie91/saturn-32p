8966c5b5c69bc5de1cf0e77772dbfd68fc65c6eb
NepDisk <yoshikirby43@gmail.com>
2023-10-20 04:13:36 -0400

    Increment version due to code change

--- src/doomdef.h
+++ src/doomdef.h
@@ -126,9 +126,9 @@ extern char  logfilename[1024];
 // we use comprevision and compbranch instead.
 #else
 #define VERSION    1 // Game version
-#define SUBVERSION 96 // more precise version number
-#define VERSIONSTRING "Planeptune 1.3a"
-#define VERSIONSTRINGW L"Planeptune 1.3a"
+#define SUBVERSION 95 // more precise version number
+#define VERSIONSTRING "Planeptune 1.b"
+#define VERSIONSTRINGW L"Planeptune 1.3b"
 // Hey! If you change this, add 1 to the MODVERSION below! Otherwise we can't force updates!
 // And change CMakeLists.txt (not src/, but in root), for CMake users!
 // AND appveyor.yml, for the build bots!
