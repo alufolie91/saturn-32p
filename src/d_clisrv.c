5fe0956d2840bffc306b39f8fef15e733d51064f
NepDisk <yoshikirby43@gmail.com>
2023-11-14 06:45:46 -0500

    Allow holepunching to be disabled via a new define plus part 2 to masterserver

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -1946,13 +1946,13 @@ static void CL_LoadReceivedSavegame(void)
 static void SendAskInfo(INT32 node)
 {
 	tic_t asktime;
-	#ifdef MASTERSERVER
+	#ifdef HOLEPUNCH
 	if (node != 0 && node != BROADCASTADDR &&
 			cv_rendezvousserver.string[0])
 	{
 		I_NetRequestHolePunch(node);
 	}
-	#endif MASTERSERVER
+	#endif
 
 	asktime = I_GetTime();
 
@@ -6255,9 +6255,9 @@ static void UpdatePingTable(void)
 	}
 }
 
+#ifdef HOLEPUNCH
 static void RenewHolePunch(void)
 {
-#ifdef MASTERSERVER
 	if (cv_rendezvousserver.string[0])
 	{
 		static time_t past;
@@ -6270,8 +6270,8 @@ static void RenewHolePunch(void)
 			past = now;
 		}
 	}
-#endif
 }
+#endif
 
 // Handle timeouts to prevent definitive freezes from happenning
 static void HandleNodeTimeouts(void)
@@ -6305,11 +6305,14 @@ void NetKeepAlive(void)
 #ifdef MASTERSERVER
 	MasterClient_Ticker();
 #endif
-
+	
+	#ifdef HOLEPUNCH
 	if (netgame && serverrunning)
 	{
 		RenewHolePunch();
 	}
+	#endif
+
 
 	if (client)
 	{
@@ -6422,10 +6425,12 @@ void NetUpdate(void)
 	MasterClient_Ticker(); // Acking the Master Server
 #endif
 
+	#ifdef HOLEPUNCH
 	if (netgame && serverrunning)
 	{
 		RenewHolePunch();
 	}
+	#endif
 
 	if (client)
 	{
--- src/m_menu.c
+++ src/m_menu.c
@@ -271,12 +271,16 @@ static menu_t SP_TimeAttackDef, SP_ReplayDef, SP_GuestReplayDef, SP_GhostDef;
 // Multiplayer
 #ifndef NONET
 static void M_PreStartServerMenu(INT32 choice);
+#ifdef MASTERSERVER
 static void M_PreStartServerMenuChoice(event_t *ev);
 static void M_PreConnectMenu(INT32 choice);
 static void M_PreConnectMenuChoice(event_t *ev);
+#endif
 static void M_StartServerMenu(INT32 choice);
+#ifdef MASTERSERVER
 static void M_ConnectMenu(INT32 choice);
 static void M_ConnectMenuModChecks(INT32 choice);
+#endif
 static void M_Refresh(INT32 choice);
 static void M_Connect(INT32 choice);
 #endif
@@ -9926,6 +9930,7 @@ static void M_Refresh(INT32 choice)
 #endif/*MASTERSERVER*/
 }
 
+#ifdef MASTERSERVER
 static void M_DrawServerCountAndHorizontalBar(void)
 {
 	const char *text;
@@ -9968,6 +9973,7 @@ static void M_DrawServerCountAndHorizontalBar(void)
 	V_DrawFill(1, currentMenu->y+32, center - radius - 2, 1, 0);
 	V_DrawFill(center + radius + 2, currentMenu->y+32, BASEVIDWIDTH - 1, 1, 0);
 }
+#endif
 
 static void M_DrawServerLines(INT32 x, INT32 page)
 {
@@ -10149,9 +10155,9 @@ void M_SortServerList(void)
 
 #ifndef NONET
 #ifdef UPDATE_ALERT
+#ifdef MASTERSERVER	
 static void M_CheckMODVersion(int id)
 {
-#ifdef MASTERSERVER	
 	char updatestring[500];
 	const char *updatecheck = GetMODVersion(id);
 	if(updatecheck)
@@ -10165,15 +10171,16 @@ static void M_CheckMODVersion(int id)
 		I_unlock_mutex(m_menu_mutex);
 #endif
 	}
-#endif MASTERSERVER
 }
+#endif //MASTERSERVER
 #endif/*UPDATE_ALERT*/
 
 #if defined (UPDATE_ALERT) && defined (HAVE_THREADS)
+#ifdef MASTERSERVER
 static void
 Check_new_version_thread (int *id)
 {
-#ifdef MASTERSERVER
+
 	M_SetWaitingMode(M_WAITING_VERSION);
 
 	M_CheckMODVersion(*id);
@@ -10186,10 +10193,12 @@ Check_new_version_thread (int *id)
 	{
 		free(id);
 	}
-#endif MASTERSERVER
+
 }
+#endif
 #endif/*defined (UPDATE_ALERT) && defined (HAVE_THREADS)*/
 
+#ifdef MASTERSERVER
 static void M_ConnectMenu(INT32 choice)
 {
 	(void)choice;
@@ -10234,6 +10243,7 @@ static void M_ConnectMenu(INT32 choice)
 #endif/*defined (MASTERSERVER) && defined (HAVE_THREADS)*/
 }
 
+
 static void M_ConnectMenuModChecks(INT32 choice)
 {
 	(void)choice;
@@ -10247,12 +10257,12 @@ static void M_ConnectMenuModChecks(INT32 choice)
 
 	M_ConnectMenu(-1);
 }
+#endif
 
 boolean firstDismissedNagThisBoot = true;
-
+#ifdef MASTERSERVER
 static void M_HandleMasterServerResetChoice(event_t *ev)
 {
-#ifdef MASTERSERVER
 	INT32 choice = -1;
 
 	choice = ev->data1;
@@ -10277,8 +10287,9 @@ static void M_HandleMasterServerResetChoice(event_t *ev)
 			}
 		}
 	}
-#endif
+
 }
+#endif
 
 static void M_PreStartServerMenu(INT32 choice)
 {
@@ -10294,17 +10305,17 @@ static void M_PreStartServerMenu(INT32 choice)
 	M_StartServerMenu(-1);
 
 }
-
+#ifdef MASTERSERVER
 static void M_PreConnectMenu(INT32 choice)
 {
 	(void)choice;
-#ifdef MASTERSERVER
+
 	if (!CV_IsSetToDefault(&cv_masterserver) && cv_masterserver_nagattempts.value > 0)
 	{
 		M_StartMessage(M_GetText("Hey! You've changed the Server Browser address.\n\nYou won't be able to see games from the official Server Browser.\nUnless you're from the future, this probably isn't what you want.\n\n\x83Press Accel\x80 to fix this and continue.\x80\nPress any other key to continue anyway.\n"),M_PreConnectMenuChoice,MM_EVENTHANDLER);
 		return;
 	}
-#endif
+
 	M_ConnectMenuModChecks(-1);
 }
 
@@ -10319,6 +10330,7 @@ static void M_PreConnectMenuChoice(event_t *ev)
 	M_HandleMasterServerResetChoice(ev);
 	M_ConnectMenuModChecks(-1);
 }
+#endif
 #endif //NONET
 
 //===========================================================================
