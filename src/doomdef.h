6ca0f790af36bd2ddac19f162ad5eb098bfd88dd
NepDisk <yoshikirby43@gmail.com>
2023-11-16 01:45:46 -0500

    Increment version

--- src/doomdef.h
+++ src/doomdef.h
@@ -126,9 +126,9 @@ extern char  logfilename[1024];
 // we use comprevision and compbranch instead.
 #else
 #define VERSION    1 // Game version
-#define SUBVERSION 91 // more precise version number
-#define VERSIONSTRING "Neptune 1.5"
-#define VERSIONSTRINGW L"Neptune 1.5"
+#define SUBVERSION 90 // more precise version number
+#define VERSIONSTRING "Neptune 1.5a"
+#define VERSIONSTRINGW L"Neptune 1.5a"
 // Hey! If you change this, add 1 to the MODVERSION below! Otherwise we can't force updates!
 // And change CMakeLists.txt (not src/, but in root), for CMake users!
 // AND appveyor.yml, for the build bots!
