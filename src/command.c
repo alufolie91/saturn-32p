ff1fa9e4e4df3d3fc23c1241610c3898f464623d
NepDisk <yoshikirby43@gmail.com>
2023-11-18 12:20:03 -0500

    Revert "Add single quotes and quote escaping to command parser"
    This reverts commit 4e43416b12e904247af819b6cf2a7182c1c6e874.

--- src/command.c
+++ src/command.c
@@ -184,7 +184,6 @@ void COM_BufExecute(void)
 	char *ptext;
 	char line[1024] = "";
 	INT32 quotes;
-	char qc = 0;
 
 	while (com_text.cursize)
 	{
@@ -194,13 +193,10 @@ void COM_BufExecute(void)
 		quotes = 0;
 		for (i = 0; i < com_text.cursize; i++)
 		{
-			if ((ptext[i] == '\"' || ptext[i] == '\'') && !quotes && i > 0 && ptext[i-1] != ' ') // Malformed command
+			if (ptext[i] == '\"' && !quotes && i > 0 && ptext[i-1] != ' ') // Malformed command
 				break;
-			if (((quotes & 1) ? (ptext[i] == qc) : (ptext[i] == '\"' || ptext[i] == '\'')) && (i == 0 || ptext[i-1] != '\\'))
-			{
+			if (ptext[i] == '\"')
 				quotes++;
-				qc = ptext[i];
-			}
 			if (!(quotes & 1) && ptext[i] == ';')
 				break; // don't break if inside a quoted string
 			if (ptext[i] == '\n' || ptext[i] == '\r')
@@ -2185,21 +2181,17 @@ skipwhite:
 	}
 
 	// handle quoted strings specially
-	if (c == '\"' || c == '\'')
+	if (c == '\"')
 	{
-		char qc = c;
 		data++;
 		for (;;)
 		{
 			c = *data++;
-			if (c == qc || c == '\0')
+			if (c == '\"' || c == '\0')
 			{
 				com_token[len] = 0;
 				return data;
 			}
-			if (c == '\\' && *data != '\0')
-				c = *data++; // parse next token literally
-
 			com_token[len] = c;
 			len++;
 		}
