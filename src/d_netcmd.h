8c8e3f56a68604a4ae208c991d81d5a06c10a30e
Alug <alufoliehorst@gmail.com>
2023-11-21 17:34:07 +0100

    toggle for localskin menus
    well wanna be able to toggle things

--- src/d_netcmd.h
+++ src/d_netcmd.h
@@ -499,6 +499,7 @@ extern consvar_t cv_showlapemblem;
 extern consvar_t cv_showviewpointtext;
 extern consvar_t cv_luaimmersion;
 extern consvar_t cv_fakelocalskin;
+extern consvar_t cv_showlocalskinmenus;
 
 extern consvar_t cv_skipmapcheck;
 
