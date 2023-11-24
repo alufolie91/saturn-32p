f4886ccde40add6f08b44646bc1df8afbb152601
NepDisk <yoshikirby43@gmail.com>
2023-11-24 10:51:00 -0500

    Change version number

--- src/doomdef.h
+++ src/doomdef.h
@@ -126,9 +126,9 @@ extern char  logfilename[1024];
 // we use comprevision and compbranch instead.
 #else
 #define VERSION    1 // Game version
-#define SUBVERSION 90 // more precise version number
-#define VERSIONSTRING "Neptune 1.5a"
-#define VERSIONSTRINGW L"Neptune 1.5a"
+#define SUBVERSION 89 // more precise version number
+#define VERSIONSTRING "Neptune 1.6"
+#define VERSIONSTRINGW L"Neptune 1.6"
 // Hey! If you change this, add 1 to the MODVERSION below! Otherwise we can't force updates!
 // And change CMakeLists.txt (not src/, but in root), for CMake users!
 // AND appveyor.yml, for the build bots!
