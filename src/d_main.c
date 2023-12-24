f8aa6d06491d8505d4ec6b4bcb14691e5460f537
Indev <bdmitry450@gmail.com>
2023-12-24 20:51:16 +0300

    Always load vanilla musicdefs

--- src/d_main.c
+++ src/d_main.c
@@ -1654,9 +1654,10 @@ void D_SRB2Main(void)
 		I_StartupSound();
 		I_InitMusic();
 		S_InitSfxChannels(cv_soundvolume.value);
-		S_InitMusicDefs();
-		S_InitMTDefs();
 	}
+
+	S_InitMusicDefs();
+	S_InitMTDefs();
 
 	CONS_Printf("ST_Init(): Init status bar.\n");
 	ST_Init();
