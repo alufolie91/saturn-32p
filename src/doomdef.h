2b63683ad55e12baac32cc0d961b99bd62108a57
NepDisk <yoshikirby43@gmail.com>
2024-02-13 21:22:55 -0500

    Version change just to be safe :)

--- src/doomdef.h
+++ src/doomdef.h
@@ -126,7 +126,7 @@ extern char  logfilename[1024];
 // we use comprevision and compbranch instead.
 #else
 #define VERSION    2 // Game version
-#define SUBVERSION 0 // more precise version number
+#define SUBVERSION 1 // more precise version number
 #define VERSIONSTRING "Neptune 1.8"
 #define VERSIONSTRINGW L"Neptune 1.8"
 // Hey! If you change this, add 1 to the MODVERSION below! Otherwise we can't force updates!
