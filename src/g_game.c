4bfc1c5555e377578f011e8378fb3be71adbf663
Alug <alufoliehorst@gmail.com>
2024-05-02 20:28:35 +0200

    fix slopes

--- src/g_game.c
+++ src/g_game.c
@@ -1671,7 +1671,7 @@ void G_DoLoadLevel(boolean resetplayer)
 	}
 
 	// Setup the level.
-	if (!P_SetupLevel(false,false))
+	if (!P_SetupLevel(false, false))
 	{
 		// fail so reset game stuff
 		Command_ExitGame_f();
