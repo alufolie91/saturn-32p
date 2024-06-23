e03cbe6e5f0cab0485a9a2dd98b3367176acf0be
NepDisk <16447892+NepDisk@users.noreply.github.com>
2024-06-23 19:53:17 -0400

    Fix intermission names offset

--- src/y_inter.c
+++ src/y_inter.c
@@ -677,10 +677,10 @@ void Y_IntermissionDrawer(void)
 				else
 					STRBUFCPY(strtime, data.match.name[i]);
 
-				if (data.match.numplayers > NUMFORNEWCOLUMN)
-					V_DrawThinString(x+36, y-1, ((data.match.num[i] == whiteplayer) ? hilicol : 0)|V_ALLOWLOWERCASE|V_6WIDTHSPACE, strtime);
-				else if (data.match.numplayers > NUMFORNEWCOLUMN*2)
+				if (data.match.numplayers > NUMFORNEWCOLUMN*2)
 					V_DrawThinString(x+18, y, ((data.match.num[i] == whiteplayer) ? hilicol : 0)|V_ALLOWLOWERCASE|V_6WIDTHSPACE, strtime);
+				else if (data.match.numplayers > NUMFORNEWCOLUMN)
+					V_DrawThinString(x+36, y-1, ((data.match.num[i] == whiteplayer) ? hilicol : 0)|V_ALLOWLOWERCASE|V_6WIDTHSPACE, strtime);
 				else
 					V_DrawString(x+36, y, ((data.match.num[i] == whiteplayer) ? hilicol : 0)|V_ALLOWLOWERCASE, strtime);
 
