60d4ea5479333f9212f62bea4198786c6a3fc546
NepDisk <16447892+NepDisk@users.noreply.github.com>
2024-07-02 16:08:05 -0400

    Revert "forgor about this stuff"
    This reverts commit 91288a999c6ab742943076447a6c9153a1150643.

--- src/p_slopes.h
+++ src/p_slopes.h
@@ -12,6 +12,8 @@
 
 #include "m_fixed.h" // Vectors
 
+#ifdef ESLOPE
+
 extern pslope_t *slopelist;
 extern UINT16 slopecount;
 
@@ -83,3 +85,4 @@ typedef struct
 
 void T_DynamicSlopeLine (dynplanethink_t* th);
 void T_DynamicSlopeVert (dynplanethink_t* th);
+#endif // #ifdef ESLOPE
