e3f8ef381de3dc7bc1e7b99932db645d90439fd8
Alug <alufoliehorst@gmail.com>
2025-08-16 16:57:03 +0200

    forgor to change the print itself

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -5415,7 +5415,7 @@ static void SV_SendTics(void)
 			}
 			packsize = bufpos - (UINT8 *)&(netbuffer->u);
 			if (packsize > MAXPACKETLENGTH)
-				CONS_Alert(CONS_ERROR, "Servertics packet went beyond allocated packet size (%ld/1024), and may have corrupted some memory...\n", packsize);
+				CONS_Alert(CONS_ERROR, "Servertics packet went beyond allocated packet size (%ld/%d), and may have corrupted some memory...\n", packsize, MAXPACKETLENGTH);
 
 			HSendPacket(n, false, 0, packsize);
 			// when tic are too large, only one tic is sent so don't go backward!
